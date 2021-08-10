6f224d93080488b65ef118fad74da0ed852918ed
Aniket Sonawane <86239912+Aniketsonawane3335@users.noreply.github.com>
2021-08-10 11:20:40 +0530

    Add files via upload

---
+++ Area.java
@@ -0,0 +1,14 @@
+class Area
+{
+public static void main (String args[])
+{
+int l,b,r,s;
+double PI=3.14;
+double area;
+
+r=4;l=5;b=6;s=8;
+
+area=PI*(r*r);
+System.out.println("area of circle of radius "+r+" \n is "+area);
+}
+}
