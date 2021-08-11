a1794b12ac9a5d9919fb0ce64eb5c90efc4d7be5
Aniket Sonawane <86239912+Aniketsonawane3335@users.noreply.github.com>
2021-08-12 00:07:27 +0530

    Create Division.java

---
+++ Division.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class Division
+{
+public static void main(String args[])
+{
+int a,b,div;
+
+Scanner d = new Scanner(System.in);
+
+System.out.println("enter the value of a ");
+a=d.nextInt();
+
+System.out.println("enter the value of  b ");
+b=d.nextInt();
+
+div=a/b;
+
+System.out.println("Division is = "+div);
+
+
+}
+}
