2cebe84d776f331be942c79211fec126361664a0
Aniket Sonawane <86239912+Aniketsonawane3335@users.noreply.github.com>
2021-08-12 00:01:44 +0530

    Create Substraction.java

---
+++ Substraction.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class Substraction
+{
+public static void main(String args[])
+{
+int a,b,sub;
+
+Scanner stud = new Scanner(System.in);
+
+System.out.println("enter the a ");
+a=stud.nextInt();
+
+System.out.println("enter the b ");
+b=stud.nextInt();
+
+sub=a-b;
+
+System.out.println("Substraction is = "+sub);
+
+
+}
+}
