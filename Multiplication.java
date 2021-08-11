952c67154fc73a963ae700cc467898692f27c0f7
Aniket Sonawane <86239912+Aniketsonawane3335@users.noreply.github.com>
2021-08-12 00:04:43 +0530

    Create Multiplication.java

---
+++ Multiplication.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class Multiplication
+{
+public static void main(String args[])
+{
+int a,b,mul;
+
+Scanner m = new Scanner(System.in);
+
+System.out.println("enter the value of a ");
+a=m.nextInt();
+
+System.out.println("enter the value of  b ");
+b=m.nextInt();
+
+mul=a*b;
+
+System.out.println("Multiplication is = "+mul);
+
+
+}
+}
