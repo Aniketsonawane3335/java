3301b15f2bac39f1736596232ed56a8fe7c80c1d
Aniket Sonawane <86239912+Aniketsonawane3335@users.noreply.github.com>
2021-08-13 10:15:37 +0530

    Add files via upload

---
+++ Data.java
@@ -0,0 +1,21 @@
+
+public class Data {
+	
+	Data ShowData()
+	{
+		System.out.println("show data called");
+		return this;
+	}
+	Data Display()
+	{
+		System.out.println("Display called");
+		return this;
+		
+		
+	}
+	Data Show()
+	{
+		System.out.println("show data");
+		return this;
+	}
+}
---
+++ Test2.java
@@ -0,0 +1,14 @@
+
+public class Test2
+{
+	
+public static void main(String args[])
+{
+	Data d=new Data();
+	
+	//d.Show().Display().ShowData();
+	
+	new Data() .Show().Display().ShowData();
+	
+}
+}
